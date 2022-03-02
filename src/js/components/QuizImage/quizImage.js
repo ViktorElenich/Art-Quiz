8e42fa93066ec61a670bd115c0898ef1ce9885b6
ViktorElenich <89132609+ViktorElenich@users.noreply.github.com>
2022-03-02 16:19:43 +0300

    feat: add component quizImage

---
+++ src/js/components/QuizImage/quizImage.js
@@ -0,0 +1,44 @@
+export class QuizImage {
+    constructor(type, imageNum, hint, correctAnswer = '') {
+        this.type = type;
+        this.imageNum = imageNum;
+        this.hint = hint;
+        this.correctAnswer = correctAnswer;
+    }
+
+    generate() {
+        return `
+        <img class="image__img" src="../../../assets/img/pictures/${this.imageNum}.jpg" width="500" height="500" />
+        <div class="image__fullscreen image__actions" title="zoom image">
+            <ion-icon name="search-outline"></ion-icon>
+        </div>
+        <div class="image__hint image__actions" title="hint">
+            <div class="tooltip btn-anim">
+                <ion-icon name="help-circle-outline"></ion-icon>
+                <div class="tooltip__content">${this.hint}</div>
+            </div>
+        </div>
+        `
+    }
+
+    render(parent) {
+        const element = document.createElement('div');
+        element.classList.add('image', 'image-loading');
+    
+        if (this.type === 'artists') {
+            element.classList.add('artists');
+        } else {
+            element.classList.add('pictures');
+        }
+    
+        element.innerHTML = this.generate();
+    
+        if (this.type === 'pictures') {
+            const image = element.querySelector('.image__img');
+            image.dataset.name = this.correctAnswer;
+            image.classList.add('answer');
+        }
+    
+        parent.append(element);
+    }
+}
